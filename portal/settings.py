cb93e70de0fc86693964378be39531aad15661b5
Gopal Gautam <gopalgautam279@gmail.com>
2023-08-31 14:21:49 +0530

    first commit

--- portal/settings.py
+++ portal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i2j++6^7+ju)#+pau8p21spoyg0uhd6$j97$%a3ecb+d=j43sw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
